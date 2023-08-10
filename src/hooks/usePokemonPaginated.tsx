739c6f805d56cc46f77bc24a37f2fb73e9fdd80b
Albert Urbina <johan50693@gmail.com>
2023-08-10 19:53:55 -0400

    Se agrega tipado a respuesta del api de pokedex

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -1,20 +1,31 @@
 /* eslint-disable prettier/prettier */
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { pokemonapi } from '../api/pokemonApi';
+import { PokemonPaginatedResponse, Result, SimplePokemon } from '../interfaces/pokemonInterfaces';
 
 export const usePokemonPaginated = () => {
 
   const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40');
+  const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([]);
 
   const loadPokemons = async () => {
 
-    const resp = await pokemonapi.get(nextPageUrl.current);
-    console.log(resp.data);
+    const resp = await pokemonapi.get<PokemonPaginatedResponse>(nextPageUrl.current);
+    nextPageUrl.current = resp.data.next;
+    mapPokemonList(resp.data.results);
 
+  };
+
+  const mapPokemonList = (pokemonList: Result[]) => {
+    pokemonList.forEach(poke => console.log(poke.name));
   };
 
   useEffect(() => {
     loadPokemons();
   }, []);
+
+  return {
+    simplePokemonList,
+  };
 
 };
